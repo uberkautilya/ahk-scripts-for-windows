8d717d6f80062039d1bf985063fb8f66841404a4
Mithun <mithuncm@gmail.com>
2023-06-14 10:30:53 +0530

    Works except for copying the content appropriately to clipboard

--- tampermonkey_101.js
+++ tampermonkey_101.js
@@ -14,19 +14,39 @@
 // actions[0].appendChild(btn);
 
 
+
+// for (let strong of strongs) {
+//     console.log(strong.textContent);
+// }
+// for (let link of hyperlinks) {
+//     console.log(link.textContent);
+// }
+
+
+
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+
 //Wait for the page to load before executing the function
 window.addEventListener('load', function () {
     'use strict';
-    let strongs = document.getElementsByTagName("strong");
-    let hyperlinks = document.getElementsByTagName("a");
-    addNormalizeButton(strongs, hyperlinks);
+    let strongs = document.getElementsByTagName('strong');
+    let hyperlinks = document.getElementsByTagName('a');
+    let ems = document.getElementsByTagName('em');
+    addNormalizeButton(strongs, hyperlinks, ems);
+
+    const underlinedTexts = document.querySelectorAll('span[style*="text-decoration: underline"]');
+    underlinedTexts.forEach(uText => uText.style.removeProperty('text-decoration'))
+
+    this.navigator.clipboard.writeText(this.document.body.innerText);
 });
 
 
 let styleSheet = `
 .copyBtn {
-    background-color: grey;
-    padding: 7px;
+    background-color: green;
+    padding: 9px;
     font-size: 12px;
 }
 `;
@@ -35,28 +55,25 @@ s.type = "text/css";
 s.innerHTML = styleSheet;
 (document.head || document.documentElement).appendChild(s);
 
-let strongs = document.getElementsByTagName("strong");
-let hyperlinks = document.getElementsByTagName("a");
-addNormalizeButton(strongs, hyperlinks);
-
 //Adds the Remove Strongs button above the date element
-function addNormalizeButton(strongs, hyperlinks) {
+function addNormalizeButton(strongs, hyperlinks, ems) {
     console.clear();
-    for (let strong of strongs) {
-        console.log(strong.textContent);
-    }
-    for (let link of hyperlinks) {
-        console.log(link.textContent);
-    }
 
     let btn = document.createElement('BUTTON');
     btn.innerHTML = "Normalize";
     btn.onclick = () => {
-        for (let strong of strongs) {
-            replaceTagWithSpan(strong);
-        }
-        for (let hyperlink of hyperlinks) {
-            replaceTagWithSpan(hyperlink);
+        let count = 0;
+        while (count < 9) {
+            for (let strong of strongs) {
+                replaceTagWithSpan(strong);
+            }
+            for (let link of hyperlinks) {
+                replaceTagWithSpan(link);
+            }
+            for (let em of ems) {
+                replaceTagWithSpan(em);
+            }
+            count = count + 1;
         }
     }
 
@@ -68,16 +85,19 @@ function addNormalizeButton(strongs, hyperlinks) {
 //Replaces the strong with a span to remove the bold effect
 function replaceTagWithSpan(element) {
     let span = document.createElement('span');
-    span.innerHTML = element.textContent;
+
     for (const attr of element.attributes) {
         span.setAttributeNS(null, attr.name, attr.value);
     }
-    for(const child of element.children){
-        span.appendChild(child);
-    }
+    span.innerHTML = element.innerHTML;
+
     element.parentNode.insertBefore(span, element);
     element.parentNode.removeChild(element);
 }
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------
 
 
 
